81e65f478a23aa31fe63d6b030618b656dfb49e3
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-12-20 15:07:05 +0100

    tui: Add DescriptionMenu component
    Just a menu with descriptions on the right of what the action does

--- tui/src/components/mod.rs
+++ tui/src/components/mod.rs
@@ -1,7 +1,9 @@
+pub mod description_menu;
 pub mod dialog;
 pub mod file_explorer;
 pub mod progress_bar;
 pub mod selectable_list;
+pub use description_menu::{DescriptionMenu, DescriptionMenuItem};
 pub use dialog::{DialogBuilder, DialogButton};
 pub use file_explorer::{ExplorerResult, FileExplorer};
 pub use progress_bar::ProgressBar;
